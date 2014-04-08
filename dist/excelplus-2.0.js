6c00e8df35901095df5b4a40f9021c2e60f2270b
Aymeric <aymeric@kodono.info>
2014-04-08 15:01:53 +0900

    tests

--- dist/excelplus-2.0.js
+++ dist/excelplus-2.0.js
@@ -659,6 +659,8 @@ var Flash = {
     * @param {String} base64 It's the base64 version of the file
     */
   getFileData: function(name, base64) {
+    alert("Flash path:"+__ExcelPlus_flashPath)
+    aler("name="+name)
     alert("here:"+base64)
     __Flash_getFileData_callback(name,base64);
   },
